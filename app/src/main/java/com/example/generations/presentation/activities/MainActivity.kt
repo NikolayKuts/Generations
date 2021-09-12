3642f15fba21b087e640c50298ae5cb360819176
NikolayKuts <survivenik@gmail.com>
2021-09-12 08:28:40 +0300

    Add MainFragment and fill it's layout

--- app/src/main/java/com/example/generations/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/generations/presentation/activities/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.generations.presentation.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.generations.R
+import com.example.generations.presentation.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, MainFragment())
+                .commit()
+        }
     }
 }
